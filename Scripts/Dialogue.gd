ba52f4b50a4d31cfe19515bcf2b53ebc2556c216
Birk Gonsholt <gonesbird@gmail.com>
2018-11-21 22:57:14 +0100

    added extensive comenting on all things

--- Scripts/Dialogue.gd
+++ Scripts/Dialogue.gd
@@ -1,8 +1,16 @@
 extends Control
 
-onready var dialogueTextLabel = $Panel/DialogueText
 
-var text = "[speed:0.5]the angry man was [pause:0.5][speed:0.1]busy being [pause:1.0][speed:0.05]angry all the [speed:0.03]time.[speed:0.15][pause:0.5] sometimes he was not that angry though[pause:0.5][speed:0.224], which was nice[pause:0.5] for him[pause:0.25] too[pause:0.3] but mostly for the rest of us."
+onready var dialogueTextLabel = $Panel/DialogueText		### this is where the dialogue is displayed :)        it's over there --->
+
+### put your commands in [] and seperate text from value with ":", examples: 
+### [pause:10] - stop the dialogue for 10 seconds,
+### [speed:0.01] - makes the text go all slow like this ... so slow ...
+### make new ones at the botom of the script
+var text = "[speed:0.2]the angry man was [pause:0.5][speed:0.1]busy being [pause:1.0][speed:0.05]angry all the [speed:0.03]time.[speed:0.15][pause:0.5] sometimes he was not that angry though[pause:0.5][speed:0.224], which was nice[pause:0.5] for him[pause:0.25] too[pause:0.3] but mostly for the rest of us."
+### above me is the example text, probably replace it and write a system for like, actually advancing the story and stuff
+
+### Index of the latest symbol being rendered to the text box :)
 var currentLetter = 0
 var isRenderingText = true
 
@@ -24,14 +32,14 @@ func _on_Timer_timeout(): ### called whenever $Timer.wait_time have elapsed
 			isRenderingText = false		### i don't know why this is here but it's probably important
 		
 		
-### this stuff work i think but i don't remember how. probably don't worry about it
-func FindCommand(textString, textPositionInt):
+### seperate the command from the rest of the text and send it to the executioner for execution :)
+func FindCommand(textString, textPositionInt):		### textString is the command like this [command:true], textPositionInt is the integer of the first symbol in the command. so like "I went to the store [speed:1000]fastly". textPositionInt would be 21 :)
 	var commandString = ""
 	var p = 0
 	
-	for c in textString:
+	for c in textString:	### loops through all the letters in textString
 		if p > textPositionInt:
-			if c == "]" and p > textPositionInt:
+			if c == "]" and p > textPositionInt:	### this means the command is over :)
 				ExecuteCommand(commandString)
 				return p
 				break

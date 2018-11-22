f2e33e6b63bc11f3b7fbe338ba6a9e357a26e6ae
Birk Gonsholt <gonesbird@gmail.com>
2018-11-22 13:13:46 +0100

    made some basic dialogue stuff

--- Scripts/Dialogue.gd
+++ Scripts/Dialogue.gd
@@ -1,26 +1,36 @@
 extends Control
 
 
-onready var dialogueTextLabel = $Panel/DialogueText		### this is where the dialogue is displayed :)        it's over there --->
+onready var dialogueTextLabel = $Panel/DialogueText		### this is where the dialogue is displayed :)     it's over there --->
 
 ### put your commands in [] and seperate text from value with ":", examples: 
 ### [pause:10] - stop the dialogue for 10 seconds,
-### [speed:0.01] - makes the text go all slow like this ... so slow ...
-### make new ones at the botom of the script
+### [speed:0.01] - makes the text go all slow like this ... so ... slow ...
+### make new ones at the botom of the script, if you want to :)
 var text = "[speed:0.2]the angry man was [pause:0.5][speed:0.1]busy being [pause:1.0][speed:0.05]angry all the [speed:0.03]time.[speed:0.15][pause:0.5] sometimes he was not that angry though[pause:0.5][speed:0.224], which was nice[pause:0.5] for him[pause:0.25] too[pause:0.3] but mostly for the rest of us.[pause:3] yay.[pause:0.4] actually,[pause:0.3] there is a secret code at the end[pause:0.3], it's[speed:0.045] 008492[speed:0.224][pause:0.2], you can use it for[pause:0.25] secret doors[pause:0.2] and[pause:0.5] other[0.45] secret[pause:0.4] things.[pause:3][speed:0.234] you know.[pause:0.7] safes[pause:0.185] and stuff."
-### above me is the example text, probably replace it and write a system for like, actually advancing the story and stuff
-
-### you actually HAVE to write it like 
+### you actually HAVE to write it like
 ### THIS: "hello[pause:1] there!". 
-### NOT: "hello [pause:1]there".	it seems small but it's a big deal!
-### THISISFINETHO: "hello[pause:1], there"	commas are fine for some reason.
+### NOT: "hello [pause:1]there".    it seems like a small change but it's a big deal!
+var book = ["[speed:0.2]hello[pause:0.5] there.", "welcome to the place[pause:1.05] of dreams.", "take a seat.", "now[pause:0.5], what brings you here?"]
+var page = 0
 
 ### Index of the latest symbol being rendered to the text box :)
 var currentLetter = 0
-var isRenderingText = true
+var isRenderingText = true		### this one is true if isRenderingText == true
 
 ### this is the previous $Timer.wait_time, we need it for [pause:] to work properly :)				[pause:] pauses things if you havent seen that, up there, it's up there. on the top of the thing
 onready var lastWaitTime = $Timer.wait_time
+
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_click") and currentLetter >= text.length() and page < book.size() - 1:
+		dialogueTextLabel.text = ""
+		page += 1
+		print("next page")
+		currentLetter = 0
+	text = book[page]
+
 
 func _on_Timer_timeout(): ### called whenever $Timer.wait_time have elapsed
 	$Timer.wait_time = lastWaitTime
@@ -33,8 +43,8 @@ func _on_Timer_timeout(): ### called whenever $Timer.wait_time have elapsed
 			dialogueTextLabel.text += text[currentLetter]
 		if currentLetter < text.length():
 			currentLetter += 1		### adding another character to the text if that's possible
-		else:
-			isRenderingText = false		### i don't know why this is here but it's probably important
+		#else:
+			#isRenderingText = false		### i don't know why this is here but it's probably important
 		
 		
 ### seperate the command from the rest of the text and send it to the executioner for execution :)
@@ -47,7 +57,7 @@ func FindCommand(textString, textPositionInt):		### textString is the command li
 			if c == "]" and p > textPositionInt:	### this means the command is over :)
 				ExecuteCommand(commandString)
 				return p
-				break	### we don't have to keep looping at this point
+				break
 			else:
 				commandString += c
 		p += 1
